b555bfa2b383e44c2e2dbb57738623e0fd38343d
sauravchugh <56736114+sauravchugh@users.noreply.github.com>
2021-11-19 22:46:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import artist from "./component/basics/artist";
+// import Artist from './component/basics/artist';
+import Restaurant from './component/basics/Restaurant';
+// import UseState from './component/Hooks/useState';
+
+
+const App = () => {
+  return <Restaurant />;
+};
+
+export default App;
+// console.log
